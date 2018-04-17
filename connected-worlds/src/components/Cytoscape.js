1fd226288c1cc60c01bee1a4eaf1cec4d34a5040
William Pearson <pearsowill@ecs.vuw.ac.nz>
2018-04-18 11:03:43 +1200

    Added test nodes

--- connected-worlds/src/components/Cytoscape.js
+++ connected-worlds/src/components/Cytoscape.js
@@ -1,8 +1,48 @@
 import React from 'react';
+import cytoscape from 'cytoscape';
 
 class Cytoscape extends React.Component {
+  constructor() {
+    super();
+    this.cyDiv = React.createRef();
+  }
+
+  componentDidMount() {
+    let cy = cytoscape({
+      container: this.cyDiv.current,
+      elements: [],
+      wheelSensitivity: 0.5
+    });
+
+    for (let i = 0; i < 10; i++) {
+      let source = 'node' + i;
+
+      cy.add({
+        data: { id: source }
+      });
+
+      if (i > 0) {
+        cy.add({
+          data: {
+            id: 'edge' + i + (i - 1),
+            source: source,
+            target: 'node' + (i - 1)
+          }
+        });
+      }
+    }
+
+    cy.add({
+      data: {
+        id: 'edgy',
+        source: 'node' + 9,
+        target: 'node' + 0
+      }
+    });
+  }
+
   render() {
-    return <div id="cy" />;
+    return <div ref={this.cyDiv} id="cy" />;
   }
 }
 
