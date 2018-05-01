dbe5e6022c5afb97349b685002ab43ab7eb77000
Jacob Cohn-Gell <jacob.cohn.gell@gmail.com>
2018-05-02 11:26:06 +1200

    Make changes to bottom bar, use React more

--- connected-worlds/src/components/BottomBar.js
+++ connected-worlds/src/components/BottomBar.js
@@ -2,6 +2,28 @@ import React from "react";
 import logo from "../assets/vic-logo.svg";
 
 class BottomBar extends React.Component {
+  Menu = [];
+  ContactButton = (
+    <div id="contact-button" onClick={this.buttonClicked}>
+      <div>
+        Submit Issue
+        <span className="cw-github" />
+      </div>
+    </div>
+  );
+  createMenuItem(Name, Text) {
+    return (
+      <li className="ui-menu-item">
+        <div>
+          {Text}
+          <span className={"cw-" + Name} />
+        </div>
+      </li>
+    );
+  }
+
+  buttonClicked() {}
+
   render() {
     return (
       <div id="navbar-bottom">
@@ -15,17 +37,19 @@ class BottomBar extends React.Component {
         </div>
         <div id="outer-header">
           <div id="header">
-            <a>Connected Worlds</a>{" "}
+            <a>Connected Worlds</a>
             <span>: Research into VR, AR and MR at Victoria</span>
             <img src={logo} alt="Connected Worlds Logo" />
           </div>
         </div>
-        <select name="contact" id="contact" defaultValue="github">
-          <option value="help" data-class="cw-help">
+        {ContactButton}
+        {/*<select name="contact" id="contact-button" defaultValue="github">
+          <option value="help" id="help" data-class="cw-help">
             Help
           </option>
           <option
             value="mailto"
+            id="mailto"
             data-class="cw-mailto"
             href="mailto:matt.plummer@vuw.ac.nz?subject=VR%20Connected%20Worlds%20Request"
           >
@@ -33,12 +57,13 @@ class BottomBar extends React.Component {
           </option>
           <option
             value="github"
+            id="github"
             data-class="cw-github"
             href="https://github.com/axbwh/VR-Network-Vis"
           >
             Submit Issue
           </option>
-        </select>
+    </select>*/}
       </div>
     );
   }
