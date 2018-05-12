34ba5bdf2427d15edca2f8dfeb36014f84850d78
Rhys Davies <daviesrhys@ecs.vuw.ac.nz>
2018-05-12 19:17:11 +1200

    Moved new files to top of directory structure and got rid of old code

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Cytoscape from "./components/Cytoscape";
+import TopBar from "./components/TopBar";
+import BottomBar from "./components/BottomBar";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TopBar />
+        <Cytoscape />
+        <BottomBar />
+      </div>
+    );
+  }
+}
+
+export default App;
