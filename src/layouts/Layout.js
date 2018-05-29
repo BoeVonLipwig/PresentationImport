0ee652a15eeed81c695536d2bab2b7f9c43b0657
William Pearson <pearsowill@ecs.vuw.ac.nz>
2018-05-30 09:39:31 +1200

    Refactored sortBy() in Layout.js

--- src/layouts/Layout.js
+++ src/layouts/Layout.js
@@ -16,53 +16,32 @@ class Layout {
   }
 
   static sortBy(arg) {
+    let order = [1, 2, 3];
     if (arg === "reverse") {
-      return function(a, b) {
-        var orderA = 0;
-        var orderB = 0;
-
-        if (a.data("role") == "Academic Staff") {
-          orderA = 3;
-        } else if (a.data("role") == "Professional Staff") {
-          orderA = 2;
-        } else {
-          orderA = 1;
-        }
-
-        if (b.data("role") == "Academic Staff") {
-          orderB = 3;
-        } else if (b.data("role") == "Professional Staff") {
-          orderB = 2;
-        } else {
-          orderB = 1;
-        }
-
-        return orderA - orderB;
-      };
-    } else {
-      return function(a, b) {
-        var orderA = 0;
-        var orderB = 0;
-
-        if (a.data("role") == "Academic Staff") {
-          orderA = 3;
-        } else if (a.data("role") == "Professional Staff") {
-          orderA = 2;
-        } else {
-          orderA = 1;
-        }
-
-        if (b.data("role") == "Academic Staff") {
-          orderB = 3;
-        } else if (b.data("role") == "Professional Staff") {
-          orderB = 2;
-        } else {
-          orderB = 1;
-        }
-
-        return orderA - orderB;
-      };
+      order = order.reverse();
     }
+    return function(a, b) {
+      var orderA = 0;
+      var orderB = 0;
+
+      if (a.data("role") == "Academic Staff") {
+        orderA = order[0];
+      } else if (a.data("role") == "Professional Staff") {
+        orderA = order[1];
+      } else {
+        orderA = order[2];
+      }
+
+      if (b.data("role") == "Academic Staff") {
+        orderB = order[0];
+      } else if (b.data("role") == "Professional Staff") {
+        orderB = order[1];
+      } else {
+        orderB = order[2];
+      }
+
+      return orderA - orderB;
+    };
   }
 }
 
