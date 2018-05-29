e6de6831e0c3fc569bdc1a33e0183ee3a6be9059
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-05-30 10:02:18 +1200

    did collab layout. now needs to make it so that keys are placed properly

--- src/layouts/CollaboratorLayout.js
+++ src/layouts/CollaboratorLayout.js
@@ -2,23 +2,23 @@ import Layout from "./Layout";
 
 class CollaboratorLayout extends Layout {
   static getLayout() {
-    this.learStyles();
-    var elesHide = this.cy.elements('[type = "project"], [type = "school"]');
-    var elesFilter = this.cy.elements('[type = "project"]');
+    this.clearStyles();
+    let elesHide = this.cy.elements('[type = "project"], [type = "school"]');
+    let elesFilter = this.cy.elements('[type = "project"]');
 
-    var activePeople = this.cy.nodes('[type = "project"]').closedNeighborhood();
-    var nonActivePeople = this.cy.nodes('[type = "person"]').not(activePeople);
+    let activePeople = this.cy.nodes('[type = "project"]').closedNeighborhood();
+    let nonActivePeople = this.cy.nodes('[type = "person"]').not(activePeople);
     elesFilter = elesFilter.add(nonActivePeople);
 
     elesHide.addClass("hidden");
     elesFilter.addClass("filtered");
 
-    var people = activePeople.nodes('[type = "person"]');
+    let people = activePeople.nodes('[type = "person"]');
 
-    var layout = people.layout({
+    let layout = people.layout({
       name: "circle",
       avoidOverlap: false,
-      // padding: layoutPadding,
+      padding: this.layoutPadding,
       radius: this.circleRadius(people),
       nodeDimensionsIncludeLabels: false,
       sort: this.sortBy("reverse")
@@ -37,45 +37,7 @@ class CollaboratorLayout extends Layout {
     // addKey.arrange();
     // this.cy.$(":selected").forEach(highlight);
 
-    return { name: "breadthfirst" };
-  }
-
-  addCollab() {
-    this.cy.nodes('[type = "project"]').forEach(function(projectNode) {
-      projectNode
-        .closedNeighborhood()
-        .nodes('[type = "person"]')
-        .forEach(function(person) {
-          projectNode
-            .closedNeighborhood()
-            .nodes('[type = "person"]')
-            .forEach(function(otherPerson) {
-              if (
-                person != otherPerson &&
-                this.cy
-                  .edges(
-                    '[id ="' + person.id() + "to" + otherPerson.id() + '"]'
-                  )
-                  .size() < 1 &&
-                this.cy
-                  .edges(
-                    '[id ="' + otherPerson.id() + "to" + person.id() + '"]'
-                  )
-                  .size() < 1
-              ) {
-                this.cy.add({
-                  group: "edges",
-                  data: {
-                    id: person.id() + "to" + otherPerson.id(),
-                    source: person.id(),
-                    target: otherPerson.id(),
-                    type: "collab"
-                  }
-                });
-              }
-            });
-        });
-    });
+    return [layout];
   }
 }
 
