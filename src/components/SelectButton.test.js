9b177c87062a5acb92208ab64e0153aab9b9987c
Rhys Davies <daviesrhys@ecs.vuw.ac.nz>
2018-06-01 15:04:55 +1200

    added tests for select button
    the tests covers the button names of what they are on the prototype which work.
    there is a bug where it does not display the checked field
    -Kyle

--- src/components/SelectButton.test.js
+++ src/components/SelectButton.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import SelectButton from "./SelectButton";
+import { shallow } from "enzyme";
 
 it("renders button without crashing", () => {
   const div = document.createElement("div");
@@ -11,14 +12,60 @@ it("renders button without crashing", () => {
 it("renders button correctly without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<SelectButton />, div);
-  ReactDOM.unmountComponentAtNode(div);
   expect(div).toMatchSnapshot();
+  ReactDOM.unmountComponentAtNode(div);
 });
 
-it("should respond to change event and change the state of the select button component", () => {
-  const wrapper = shallow(<SelectButton id="showSchools" checked={true} />);
-  expect(wrapper.state("showSchools")).toEqual(true);
+it("renders button correctly as false", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<SelectButton isChecked={false} />, div);
+
+  expect(div).toMatchSnapshot();
+  ReactDOM.unmountComponentAtNode(div);
 });
+
+it("renders button correctly as true", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<SelectButton isChecked={true} />, div);
+  expect(div).toMatchSnapshot();
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders button correctly as correct name 'Projects'", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<SelectButton name="Projects" />, div);
+  expect(div).toMatchSnapshot();
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders button correctly as correct name 'Programme'", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<SelectButton name="Programme" />, div);
+  expect(div).toMatchSnapshot();
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders button correctly as correct name 'Collaborators'", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<SelectButton name="Collaborators" />, div);
+  expect(div).toMatchSnapshot();
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it("renders button correctly as correct name 'Show Details'", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<SelectButton name="Show Details" />, div);
+  expect(div).toMatchSnapshot();
+  ReactDOM.unmountComponentAtNode(div);
+});
+//
+// it("should respond to change event and change the state of the select button component", () => {
+//   const wrapper = shallow(
+//     <SelectButton id="showSchools" checked={true} />
+//   ).find("showSchools");
+//   // expect(wrapper.toEqual(null));
+//   expect(wrapper.dive().state("showSchools")).toEqual(true);
+// });
 
 // it("should respond to change event and change the state of the select button component", () => {
 //   const wrapper = shallow(<SelectButton id="showSchools" checked=true />);
