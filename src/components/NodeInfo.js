d8f010b63b0c4c68af9d1ab2977265e14d56f3e2
shaun <sdmsinclair@gmail.com>
2018-06-06 09:39:21 +1200

    no idea

--- src/components/NodeInfo.js
+++ src/components/NodeInfo.js
@@ -1,38 +1,70 @@
-import React from "react";
+import React, { Fragment } from "react";
 import cytoscapeStore from "../util/CytoscapeStore";
 import "./NodeInfo.css";
 
 class NodeInfo extends React.Component {
   render() {
     let node = cytoscapeStore.node;
-    return <div>{this.nodeData(node)}</div>;
-  }
 
-  nodeData(node) {
     let infoTitle = node.data("name");
     let brief = node.data("brief");
     let infoSchool = node.data("school");
     let mediaLink = node.data("mediaLink");
     let siteLink = node.data("siteLink");
     let staffSiteLink = node.data("staffSiteLink");
-    let siteName = !node.data("siteName")
-      ? node.data("siteName")
-      : node.data("name");
+    let siteName = node.data("siteName");
     let nodeType = node.data("type");
     let role = node.data("role");
     let datesActive = node.data("datesActive");
 
-    if (!mediaLink && nodeType === "person") {
+    // if (nodeType === "person") return parsePerson();
+    let link;
+    let fullInfo = "";
+    if (nodeType === "person" && !mediaLink) {
       mediaLink = "assets/id-img.png";
     }
 
+    if (!siteName) {
+      siteName = node.data("name");
+    }
+
+    /**
+     <div className="info-row">
+     <p className="info-left"> |</p>
+     <p className="info-right">{}</p>
+     </div>
+     */
+
+    /**
+     <a href = "link" >text</a>
+     */
+
     return (
-      <div className="info-row">
-        <hr />
-        <p className="info-brief">{brief}</p>
-      </div>
+      <Fragment>
+        {this.parseRole(role)}
+        <div className="info-row">
+          <p className="info-left">Programme |</p>
+          <p className="info-right">{infoSchool}</p>
+        </div>
+        <div className="info-row">
+          <p className="info-left">Website |</p>
+          <p className="info-right">{siteName}</p>
+        </div>
+        <div className="info-row">
+          <hr />
+          <p className="info-brief">{brief}</p>
+        </div>
+      </Fragment>
     );
   }
-}
 
+  parseRole(role) {
+    return role ? (
+      <div className="info-row">
+        <p className="info-left">Role |</p>
+        <p className="info-right">{role}</p>
+      </div>
+    ) : null;
+  }
+}
 export default NodeInfo;
