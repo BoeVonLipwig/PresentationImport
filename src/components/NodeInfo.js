039840df6a2d166f2d4881fa1220b93c3fbd2335
Rhys Davies <daviesrhys@ecs.vuw.ac.nz>
2018-06-14 00:23:31 +1200

    Fixed image links to go to correct location

--- src/components/NodeInfo.js
+++ src/components/NodeInfo.js
@@ -26,7 +26,7 @@ class NodeInfo extends React.Component {
 
     return (
       <Fragment>
-        {this.parseMedia(mediaLink)}
+        {this.parseMedia(mediaLink, staffSiteLink)}
         {this.parseRole(role)}
         {this.parseProgram(infoSchool)}
         {this.parseSite(siteName, siteLink, staffSiteLink)}
@@ -36,7 +36,7 @@ class NodeInfo extends React.Component {
     );
   }
 
-  parseMedia(mediaLink) {
+  parseMedia(mediaLink, staffSiteLink) {
     let pattern1 = /(?:http?s?:\/\/)?(?:www\.)?(?:vimeo\.com)\/?(.+)/g;
     let pattern2 = /(?:http?s?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g;
 
@@ -79,7 +79,7 @@ class NodeInfo extends React.Component {
 
     return (
       <div className="id-wrapper id-linked">
-        <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+        <a href={staffSiteLink} target="_blank" rel="noopener noreferrer">
           <img
             className="img-crop"
             src={mediaLink}
