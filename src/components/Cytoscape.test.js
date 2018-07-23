a6ad37b4ae4ce68ba0836c4f9bb75520b4e4a120
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-07-23 15:25:40 +1200

    removed console logs which are not needed for the tests

--- src/components/Cytoscape.test.js
+++ src/components/Cytoscape.test.js
@@ -4,19 +4,15 @@ import Cytoscape from "./Cytoscape";
 
 // 1.
 it("renders cytoscape without cytoscape crashing", () => {
-  console.log("HI 1");
   const div = document.createElement("div");
   ReactDOM.render(<Cytoscape />, div);
   ReactDOM.unmountComponentAtNode(div);
-  console.log("BYE 1");
 });
 
 // 2.
 it("renders cytoscape correctly", () => {
-  console.log("HI 2");
   const div = document.createElement("div");
   ReactDOM.render(<Cytoscape />, div);
   expect(div).toMatchSnapshot();
   ReactDOM.unmountComponentAtNode(div);
-  console.log("BYE 2");
 });
