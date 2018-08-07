a3a9e03303f1d7fd20e45b39e507c373bd12035e
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-08-07 16:03:21 +1200

    started on load data method in parser. it makes node objects correctly with old data (progress for issue #78)

--- parser/csv_parser.py
+++ parser/csv_parser.py
@@ -1,34 +1,75 @@
 import sys
+import csv
 
 class Node:
     # webLink -> "hdawuidhaw.com"
     # bio -> "adawoiudoiudb"
-    def __init__(self, name, fields):
+    def __init__(self, id, name, fields):
+        self.id = id
         self.name = name
         self.fields = fields
 
     def equals(self, other):
-        return self.name == otherObject.name
+        return self.name == other.name
+
+    def __repr__(self):
+        return "ID: %d \nName: %s\nFields: %s\n\n" % (self.id,self.name,self.fields)
 
 
 class Edge:
-    def __init__(self, node1, node2):
+    def __init__(self, id, node1, node2):
+        self.id = id
         self.node1 = node1
         self.node2 = node2
 
     def equals(self, other):
         return (self.node1 == other.node1 and self.node2 == other.node2) or (self.node1 == other.node2 and self.node2 == other.node1)
 
+    def __repr__(self):
+        return "ID: %d\nNode1: %s\nNode2: %s\n\n" % (self.id,self.node1,self.node2)
+
+
+def getFileNames():
+    filenames = [arg for arg in sys.argv]
+    del filenames[0] # first entry is parser name
+    return filenames
+
+
+def extractFileIntoList(file):
+    with open(file,'r') as f:
+        reader = csv.reader(f)
+        instances = list(reader)
+    return instances[1:], instances[0]
+
+
+def createNodesFromFile(ID,file):
+    nodes = list()
+    instances,metaData = extractFileIntoList(file)
+    for i in instances:
+        name = i[0]
+        fields = dict(zip(metaData[1:], i[1:]))
+        nodes.append(Node(ID,name,fields))
+        ID+=1
+
+    print(metaData)
+    print(nodes[0])
+
+
+
+def createNodes(fn):
+    ID = 1
+    for file in fn:
+        createNodesFromFile(ID,file)
+
 
 def loadData():
-    filenames = [arg for arg in sys.argv]
-    del filenames[0]
+    filenames = getFileNames()
+    nodes = createNodes(filenames)
 
-    print(filenames)
+    # check validity of data
 
-        # check validity of data
 
-        # Create edge objects
+    # Create edge objects
 
 
 def formatForCytoscape():
@@ -41,5 +82,5 @@ def generateOutputFile():
 
 if __name__ == '__main__':
     loadData()
-    formatForCytoscape()
-    generateOutputFile()
+    # formatForCytoscape()
+    # generateOutputFile()
