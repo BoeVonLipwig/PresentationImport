574ced8e159eb32949ff998e1bba4d3dd57b3117
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-08-07 14:11:02 +1200

    set up stub functions for the parser and planned skeleton for parser. William and Kyle. (progress for issue #78)

--- parser/csv_parser.py
+++ parser/csv_parser.py
@@ -1,5 +1,45 @@
+import sys
 
+class Node:
+    # webLink -> "hdawuidhaw.com"
+    # bio -> "adawoiudoiudb"
+    def __init__(self, name, fields):
+        self.name = name
+        self.fields = fields
+
+    def equals(self, other):
+        return self.name == otherObject.name
+
+
+class Edge:
+    def __init__(self, node1, node2):
+        self.node1 = node1
+        self.node2 = node2
+
+    def equals(self, other):
+        return (self.node1 == other.node1 and self.node2 == other.node2) or (self.node1 == other.node2 and self.node2 == other.node1)
+
+
+def loadData():
+    filenames = [arg for arg in sys.argv]
+    del filenames[0]
+
+    print(filenames)
+
+        # check validity of data
+
+        # Create edge objects
+
+
+def formatForCytoscape():
+    print("stub")
+
+
+def generateOutputFile():
+    print("stub")
 
 
 if __name__ == '__main__':
-    # data = sys.argv[1]  # first file name is training
+    loadData()
+    formatForCytoscape()
+    generateOutputFile()
