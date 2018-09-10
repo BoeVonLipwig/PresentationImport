70242d31d012a85c804d568efc68f458f69194ed
Kyle Claudio <claudikyle@ecs.vuw.ac.nz>
2018-09-10 16:48:35 +1200

    website uses output.json instead of load.js for getting data. network request class now has a set of filter names #91

--- src/util/DataFilter.js
+++ src/util/DataFilter.js
@@ -1,13 +1,15 @@
-import loadData from "../util/data";
-import filter from "../util/DataFilter";
-
 // This class gets the data and finds name filters for cytoscape
 class DataFilter {
   parseElementsToGetFilterNames(graphP) {
-    filterNames = new Set();
-    for (let dataInstances in graphP) {
-      filterNames.add(dataInstances.data.type);
-    }
+    let filterNames = graphP.then(graphData => {
+      let set = new Set();
+      for (let i = 0; i < graphData.length; i++) {
+        let dataInstance = graphData[i];
+        set.add(dataInstance.data.type);
+      }
+      console.log(set);
+      return set;
+    });
     return filterNames;
   }
 }
