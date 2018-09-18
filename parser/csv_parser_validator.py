628790fdacfe561ca8f631d3dc274f138500241f
Shaun Sinclair <sdmsinclair@gmail.com>
2018-09-18 13:42:31 +1200

    test lint fixes

--- parser/csv_parser_validator.py
+++ parser/csv_parser_validator.py
@@ -1,8 +1,8 @@
 from csv_parser_file_functions import extractFileIntoList, getRawLineFromFile
 
 
-def validate_data(specialNodes, normalNodes, modifierNodes, numberOfSpecialNodes, lastDetailsFieldIndex, specialFN, 
-nodesFN, viewsFN, specialNames):
+def validate_data(specialNodes, normalNodes, modifierNodes, numberOfSpecialNodes, lastDetailsFieldIndex, specialFN,
+    nodesFN, viewsFN, specialNames):
     if not check_case1(specialNodes, normalNodes, lastDetailsFieldIndex, numberOfSpecialNodes, nodesFN):
         print("case1 failed")
         return False
