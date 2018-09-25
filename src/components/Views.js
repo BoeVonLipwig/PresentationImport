1eda00c282b09f297e5c197aefe7b2cf8ac9b0a2
Shaun Sinclair <sdmsinclair@gmail.com>
2018-09-25 19:59:16 +1200

    cytoscapeStore.selectedNode is a index for the node storage and as such the name cant be pulled from it. fixed by creating a node variable and fixing the turneries and data access to run off them. Also fixed some heavily redundent code in view.js clickHandler().

--- src/components/Views.js
+++ src/components/Views.js
@@ -50,16 +50,8 @@ class Views extends React.Component {
 
   clickHandler(e, id) {
     this.toggleCheck(id);
-    if (
-      cytoscapeStore.selectedNode != null &&
-      cytoscapeStore.visNodesMap[cytoscapeStore.selectedNode] === null
-    ) {
+    if (cytoscapeStore.visNodesMap[cytoscapeStore.selectedNode] === null)
       cytoscapeStore.selectedNode = null;
-    } else {
-      let node = cytoscapeStore.selectedNode;
-      cytoscapeStore.selectedNode = null;
-      cytoscapeStore.selectedNode = node;
-    }
   }
 
   render() {
