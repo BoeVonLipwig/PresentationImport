744d926f8ef8adbe57b5d65f272462fddc37e1fd
Shaun Sinclair <sdmsinclair@gmail.com>
2018-10-18 23:23:43 +1300

    view name change

--- src/components/Views.js
+++ src/components/Views.js
@@ -34,10 +34,8 @@ class Views extends React.Component {
   toggleCheck(id) {
     const newViews = this.state.views.map(function(entry) {
       if (entry.id === id) {
-        if (!entry.isChecked) {
-          cytoscapeStore.layoutID = entry.id;
-          cytoscapeStore.layouts = layoutFactory.computeLayout(entry.id);
-        }
+        cytoscapeStore.layoutID = entry.id;
+        cytoscapeStore.layouts = layoutFactory.computeLayout(entry.id);
         return Object.assign({}, entry, {
           isChecked: true
         });
