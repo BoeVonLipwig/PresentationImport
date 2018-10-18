1b2ad68740067f0c5dd51af73b4320e6b89bb8af
Rhys Davies <daviesrhys@ecs.vuw.ac.nz>
2018-10-19 12:45:50 +1300

    Revert "Add build time rendering to speed up page loading"
    This reverts commit 4ee3cbb6636212798e03da70092a76a86fa31d85.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
-import "./index.css";
 import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import { hydrate, render } from "react-dom";
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(<App />, rootElement);
-} else {
-  render(<App />, rootElement);
-}
-
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
